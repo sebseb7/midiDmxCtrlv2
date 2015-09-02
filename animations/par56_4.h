3abefcc421b090c95ed842e663d713e0dd58d8c2
sebseb7 <github@seb.exse.net>
2015-09-02 21:34:00 +0200

    scale wrapping

--- animations/par56_4.h
+++ animations/par56_4.h
@@ -4,26 +4,28 @@
 
 #include "sini.h"
 
-static int e;
+static int e=0;
+static int e2=0;
 
 static int last_la;
 
 static uint8_t tick_5(void) 
 {
 
-	float var = 0.5f+((sini(e*80)/64535.0f)*0.7f);
+	float var = 0.5f+((sini(e2)/64535.0f)*0.5f);
 
 	setpar(last_la,0,0);
 	setpar(last_la,1,0);
 	setpar(last_la,2,0);
 //	setpar(5-last_la,2,0);
-	last_la = sini(e*450*var)/65535.0f*6;
+	last_la = sini(e)/65535.0f*6;
 	setpar(last_la,0,255);
 	setpar(last_la,1,255);
 	setpar(last_la,2,255);
 //	setpar(5-last_la,2,255);
 	
-	e++;
+	e+=1200*var;
+	e2+=80;
 	return 1;
 }
 
